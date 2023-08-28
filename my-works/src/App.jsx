fc4a6b16a2c2209984e2575f92dd8130ffc14601
BatuhanOkur <batuhanokur2209@gmail.com>
2023-08-29 00:56:40 +0300

    course app

---
+++ my-works/src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+import  Course  from './Course'
+
+function App() {
+
+  return (
+    <>
+      <Course 
+      title="Bookstore"
+      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam delectus beatae possimus 
+      harum illum dignissimos illo ratione voluptas eum placeat ipsam non et optio consectetur quaerat, odit assumenda, repellendus corporis."/>
+      <Course 
+      title="ERP"
+      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam delectus beatae possimus 
+      harum illum dignissimos illo ratione voluptas eum placeat ipsam non et optio consectetur quaerat, odit assumenda, repellendus corporis."/>
+      <Course
+      title="MES"
+      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam delectus beatae possimus 
+      harum illum dignissimos illo ratione voluptas eum placeat ipsam non et optio consectetur quaerat, odit assumenda, repellendus corporis."/>
+      <Course
+      title="Scarp"
+      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam delectus beatae possimus 
+      harum illum dignissimos illo ratione voluptas eum placeat ipsam non et optio consectetur quaerat, odit assumenda, repellendus corporis."/>
+      
+    </>
+  )
+}
+
+export default App
---
+++ my-works/src/Course.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+
+function Course ({title, description}) { 
+  return ( 
+    <>
+      <div>{title}</div>
+      <div>{description}</div>
+    </>
+   );
+}
+
+export default Course;
