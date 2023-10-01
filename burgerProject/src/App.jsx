f61c5812aedad96ad8de3f14854563899b6e168a
BatuhanOkur <batuhanokur2209@gmail.com>
2023-10-01 23:57:27 +0300

    navbar design added

--- burgerProject/src/App.jsx
+++ burgerProject/src/App.jsx
@@ -4,18 +4,20 @@ import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
 import Menu from './components/Menu';
+import Navbar from './components/Navbar';
 
 function App() {
 
 
   return (
     <>
-     <Routes>
-      <Route path='/' exact element={<Home/>}/>
-      <Route path='/about' exact element={<About/>}/>
-      <Route path='/contact' exact element={<Contact/>}/>
-      <Route path='/menu' exact element={<Menu/>}/>
-     </Routes>
+      <Navbar/>
+      <Routes>
+        <Route path='/' exact element={<Home/>}/>
+        <Route path='/about' exact element={<About/>}/>
+        <Route path='/contact' exact element={<Contact/>}/>
+        <Route path='/menu' exact element={<Menu/>}/>
+      </Routes>
     </>
     
   )
