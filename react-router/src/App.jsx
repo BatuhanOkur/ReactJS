83679d69d6230243c67179d0343840634e6dfc39
BatuhanOkur <batuhanokur2209@gmail.com>
2023-10-01 23:22:33 +0300

    lazy loading usage added

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import {Routes, Route} from 'react-router-dom'
 import Home from './components/Home'
-import AboutUs from './components/AboutUs'
+//import AboutUs from './components/AboutUs'
 import Navbar from './components/Navbar'
 import Profile from './components/Profile'
 import WrongPath from './components/WrongPath'
@@ -10,6 +10,8 @@ import OurCompanies from './components/OurCompanies'
 import Team from './components/Team'
 import Users from './components/Users'
 import UserDetails from './components/UserDetails'
+import React from 'react'
+const LazyAboutUs = React.lazy(() => import('./components/AboutUs'));
 
 function App() {
   
@@ -19,7 +21,11 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>} />
-        <Route path='/about-us' element={<AboutUs/>} />
+        <Route path='/about-us' element={
+        <React.Suspense>
+          <LazyAboutUs/>
+        </React.Suspense>
+        } />
         <Route path='/profile' element={<Profile/>} />
         <Route path='/history' element={<History/>}>
           <Route path='our-companies' element={<OurCompanies/>}/>
--- react-router/src/components/AboutUs.jsx
+++ react-router/src/components/AboutUs.jsx
@@ -1,6 +1,15 @@
 function AboutUs() {
     return ( <>
-    About Us
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est eaque unde maiores aliquid, quod, voluptas facere illum hic magni laboriosam deserunt voluptate quo culpa laborum nobis veniam totam! Nulla, culpa.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Est eaque unde maiores aliquid, quod, voluptas facere illum hic magni laboriosam deserunt voluptate quo culpa laborum nobis veniam totam! Nulla, culpa.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Est eaque unde maiores aliquid, quod, voluptas facere illum hic magni laboriosam deserunt voluptate quo culpa laborum nobis veniam totam! Nulla, culpa.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Est eaque unde maiores aliquid, quod, voluptas facere illum hic magni laboriosam deserunt voluptate quo culpa laborum nobis veniam totam! Nulla, culpa.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Est eaque unde maiores aliquid, quod, voluptas facere illum hic magni laboriosam deserunt voluptate quo culpa laborum nobis veniam totam! Nulla, culpa.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Est eaque unde maiores aliquid, quod, voluptas facere illum hic magni laboriosam deserunt voluptate quo culpa laborum nobis veniam totam! Nulla, culpa.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Est eaque unde maiores aliquid, quod, voluptas facere illum hic magni laboriosam deserunt voluptate quo culpa laborum nobis veniam totam! Nulla, culpa.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Est eaque unde maiores aliquid, quod, voluptas facere illum hic magni laboriosam deserunt voluptate quo culpa laborum nobis veniam totam! Nulla, culpa.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Est eaque unde maiores aliquid, quod, voluptas facere illum hic magni laboriosam deserunt voluptate quo culpa laborum nobis veniam totam! Nulla, culpa.
+    </div>
     </> );
 }
 
