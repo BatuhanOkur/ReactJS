d88faf0cd5e04fa9f1099b57c4dab7834a774678
BatuhanOkur <batuhanokur2209@gmail.com>
2023-10-01 23:35:30 +0300

    burger project added and route design completed

---
+++ burgerProject/src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+import {Route,Routes} from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Menu from './components/Menu';
+
+function App() {
+
+
+  return (
+    <>
+     <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+      <Route path='/menu' element={<Menu/>}/>
+     </Routes>
+    </>
+    
+  )
+  
+}
+
+export default App
