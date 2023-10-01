673cb7d7927110d42ded2ccf13f09f86c1400d96
BatuhanOkur <batuhanokur2209@gmail.com>
2023-10-01 23:18:18 +0300

    useParams and useSearchParams usage added

--- react-router/src/components/UserDetails.jsx
+++ react-router/src/components/UserDetails.jsx
@@ -1,8 +1,11 @@
-
+import { useParams } from "react-router-dom"
 
 function UserDetails() {
+  // const params = useParams();
+  // const userid = params.userId;
+  const {userId} = useParams();
   return (
-    <div>UserDetails</div>
+    <div>UserDetails {userId}</div>
   )
 }
 
--- react-router/src/components/Users.jsx
+++ react-router/src/components/Users.jsx
@@ -1,8 +1,15 @@
-
+import { useSearchParams } from "react-router-dom"
 
 function Users() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const isActive = searchParams.get('filter') === 'active';
   return (
-    <div>Users</div>
+    <>
+      <div>Users</div>
+      <button onClick={() => setSearchParams({filter:'active'})}>Aktif Uyeler</button>
+      <button onClick={() => setSearchParams()}>Tüm Uyeler</button>
+      {isActive ? <h2>Aktif üyeler</h2> : <h2>Tüm Üyeler</h2>}
+    </>
   )
 }
 
