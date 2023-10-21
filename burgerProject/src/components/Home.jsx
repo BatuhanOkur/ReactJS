840856582069e13c9b0a5f864789ebce41c41c60
BatuhanOkur <batuhanokur2209@gmail.com>
2023-10-21 11:09:22 +0300

    home component added

--- burgerProject/src/components/Home.jsx
+++ burgerProject/src/components/Home.jsx
@@ -1,8 +1,18 @@
-
+import {Link} from 'react-router-dom';
+import BannerImage from '../assets/banneryeni.jpg';
+import '../style/Home.css';
 
 function Home() {
   return (
-    <div></div>
+    <>
+      <div className="mainPage" style={{backgroundImage: `url(${BannerImage})`}}	>
+        <div className='order'>
+          <Link to="/menu">
+            <button>SİPARİŞ VER</button>
+          </Link>
+        </div>
+      </div>
+    </>
   )
 }
 
