b0e2750a0da67c013dd2018e5cb62958cf961abd
BatuhanOkur <batuhanokur2209@gmail.com>
2023-10-21 11:41:27 +0300

    menu added

--- burgerProject/src/components/Menu.jsx
+++ burgerProject/src/components/Menu.jsx
@@ -1,8 +1,27 @@
-
+import {Data} from '../helpers/Data';
+import MenuItem from '../components/MenuItem';
+import '../style/Menu.css';
 
 function Menu() {
   return (
-    <div>Menu</div>
+    <>
+      <div className='menu'>
+          <h1 className='menuTitle'>Ürünlerimiz</h1>
+          <div className="menuList">
+            {Data.map((menuItem,index) => {
+              return (
+                <MenuItem 
+                key={index}
+                image = {menuItem.image}
+                name = {menuItem.name}
+                content = {menuItem.content}
+                price = {menuItem.price}
+                />
+              )
+            })}
+          </div>
+      </div>
+    </>
   )
 }
 
