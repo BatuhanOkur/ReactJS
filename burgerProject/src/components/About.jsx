f58eadbe17f7df524db2c2e40331ad4e9f2cdae6
BatuhanOkur <batuhanokur2209@gmail.com>
2023-11-06 22:59:20 +0300

    update

--- burgerProject/src/components/About.jsx
+++ burgerProject/src/components/About.jsx
@@ -1,7 +1,18 @@
-
+import BannerImage from '../assets/banneryeni.jpg'
+import '../style/About.css'
 function About() {
   return (
-    <div>About</div>
+    <>
+    <div className="about">
+      <div className="aboutTop" 
+      style={{backgroundImage: `url(${BannerImage})`}}>
+      </div>
+      <div className='aboutBottom'>
+        <h1>Hakkımızda</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis numquam officia eligendi quibusdam, perspiciatis mollitia beatae reprehenderit porro eum ab, autem, vel excepturi saepe? Quaerat aliquam non quae placeat ut? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus quo ut tempora fugiat cumque deleniti molestias, consequuntur officia ad, quisquam quia earum error autem commodi sunt explicabo dolorum veniam. Fuga. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius natus quam dignissimos illo porro architecto nulla, aperiam possimus delectus voluptate quae quasi quia iste atque repudiandae sapiente. Beatae, quod sapiente!</p>
+      </div>
+    </div>
+    </>
   )
 }
 
--- burgerProject/src/components/Contact.jsx
+++ burgerProject/src/components/Contact.jsx
@@ -1,8 +1,25 @@
-
-
+import Banner from '../assets/banner.png'
+import '../style/Contact.css'
 function Contact() {
   return (
-    <div>Contact</div>
+    <>
+    <div className="contact">
+      <div className="leftSide" 
+      style={{backgroundImage: `url(${Banner})`}}>
+      </div>
+      <div className="rightSide">
+        <h1>Bizimle İletişime Geçin</h1>
+        <form>
+          <label>Ad Soyad</label>
+          <input type="text" name='name' placeholder='Adınızı ve Soyadınızı Giriniz...' />
+          <label>Email</label>
+          <input type="email" name='email' placeholder='Emailinizi Giriniz...' />
+          <label>Mesajınız</label>
+          <textarea name="message" rows="6" placeholder='Mesajınızı Giriniz...'></textarea>
+        </form>
+      </div>
+    </div>
+    </>
   )
 }
 
